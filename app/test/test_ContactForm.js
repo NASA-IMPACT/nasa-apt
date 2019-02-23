dcab8dc51a21fef7b4c0fb01e2b4ea71259c43fe
Sean Harkins <sharkinsgis@gmail.com>
2019-02-23 12:45:55 -0500

    Tests for ContactForm validation.

--- app/test/test_ContactForm.js
+++ app/test/test_ContactForm.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import test from 'tape';
+import sinon from 'sinon';
 import { InnerContactForm } from '../src/components/ContactForm';
+
+const proxyquire = require('proxyquire').noCallThru();
 
 configure({ adapter: new Adapter() });
 
@@ -27,5 +30,24 @@ test('InnerContactForm submit disabled', (t) => {
   wrapper = shallow((<InnerContactForm {...props} />));
   submit = wrapper.find('button');
   t.notOk(submit.props().disabled, 'Submit enabled when there are no errors');
+  t.end();
+});
+
+test('ContactForm validation', (t) => {
+  const transformErrors = sinon.stub().returns({});
+  const validateEmail = sinon.stub().returns(false);
+  const email = 'Email';
+  const { ContactForm } = proxyquire(
+    '../src/components/ContactForm',
+    {
+      '../schemas/transformErrors': transformErrors,
+      '../schemas/validateEmail': validateEmail
+    }
+  );
+
+  const wrapper = shallow((<ContactForm />));
+  const instance = wrapper.instance();
+  const errors = instance.validate({ contact_mechanism_type: email });
+  t.ok(errors.contact_mechanism_value, 'Adds error when email is invalid');
   t.end();
 });
