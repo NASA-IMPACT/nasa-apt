e6b052f3bbbcc4afd251de1ed2e680384d1ad498
Ricardo Mestre <ricardo@duplos.org>
2019-03-21 14:38:09 +0000

    Create sandbox view

---
+++ app/src/components/sandbox/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Sandbox extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Sandbox</h1>
+        <div>
+          Ad duis labore adipisicing ut mollit aliqua fugiat consequat occaecat
+          commodo labore. Lorem qui enim commodo ullamco nisi sint ullamco.
+          Adipisicing ad veniam fugiat dolore eu incididunt commodo. Et
+          cupidatat anim occaecat incididunt exercitation consequat occaecat
+          veniam magna laboris dolore aliquip. Commodo incididunt fugiat aute
+          magna ex minim sint consectetur.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sandbox;
