f576d20d258d0fb2f46f148d3d80135f3cf4fe6a
Ricardo Mestre <ricardo@duplos.org>
2019-03-21 14:36:42 +0000

    Constrain footer content

--- app/src/components/common/PageFooter.js
+++ app/src/components/common/PageFooter.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import { themeVal } from '../../styles/utils/general';
+import Constrainer from '../../styles/atoms/constrainer';
 
 const PageFoot = styled.footer`
   padding: ${themeVal('layout.space')};
   background-color: ${themeVal('color.shadow')};
+  font-size: 0.875rem;
+  line-height: 1rem;
+`;
+
+export const Inner = styled(Constrainer)`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+
+  * {
+    opacity: 0.64;
+  }
 `;
 
 class PageFooter extends React.PureComponent {
   render () {
     return (
       <PageFoot>
-        <p>Copyright goes here.</p>
+        <Inner>
+          <p>Copyright goes here.</p>
+        </Inner>
       </PageFoot>
     );
   }
