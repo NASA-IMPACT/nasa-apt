8570b77ea8c9f10b94313b1ef602f4c00ad292e8
Derek Lieu <dl@dereklieu.com>
2019-03-25 21:49:24 -0700

    Include dummy dropdowns for sorting, filtering

--- app/src/components/AtbdList.js
+++ app/src/components/AtbdList.js
@@ -3,18 +3,49 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { themeVal } from '../styles/utils/general';
-import { multiply } from '../styles/utils/math';
+import { multiply, divide } from '../styles/utils/math';
+import collecticon from '../styles/collecticons';
 import PageSubNav, {
   SubNavTitle,
   SubNavFilter,
   SubNavActions,
   SubNavAction
 } from './common/PageSubNav';
+import Dropdown from './Dropdown';
 
 const Separator = styled.span`
-  border-right: 1px solid ${themeVal('color.shadow')};
+  border-right: 1px solid ${themeVal('color.lightgray')};
   padding-right: ${multiply(themeVal('layout.space'), 2)};
   margin-right: ${multiply(themeVal('layout.space'), 2)};
+`
+
+const DropdownTrigger = styled.a`
+  color: #FFF;
+  font-weight: bold;
+  padding: ${themeVal('layout.space')};
+  &::after {
+    margin-left: ${divide(themeVal('layout.space'), 2)};
+    ${collecticon('chevron-down--small')};
+  }
+`
+
+const DropdownList = styled.ul`
+  background-color: ${themeVal('color.background')};
+  border-radius: ${divide(themeVal('layout.space'), 4)};
+  box-shadow: 0 0 0 1px rgba(0,0,0,.08), 0 4px 16px 2px rgba(0,0,0,.08);
+
+  padding: ${divide(themeVal('layout.space'), 2)} 0;
+  text-align: center;
+`
+
+const DropdownItem = styled.li`
+  background-color: ${themeVal('color.background')};
+  cursor: pointer;
+  padding: ${divide(themeVal('layout.space'), 4)} ${themeVal('layout.space')};
+  transition: background-color .16s ease;
+  &:hover {
+    background-color: ${themeVal('color.shadow')};
+  }
 `
 
 const AtbdList = (props) => {
@@ -27,7 +58,56 @@ const AtbdList = (props) => {
     <Fragment>
       <PageSubNav>
         <Separator><SubNavTitle>Documents</SubNavTitle></Separator>
-        <SubNavFilter></SubNavFilter>
+
+        <SubNavFilter>
+          Status
+          <Dropdown
+            triggerText='All'
+            triggerTitle='Toggle menu options'
+            triggerElement={DropdownTrigger}>
+            <DropdownList role='menu'>
+              <DropdownItem>All</DropdownItem>
+              <DropdownItem>Published</DropdownItem>
+              <DropdownItem>Draft</DropdownItem>
+            </DropdownList>
+          </Dropdown>
+        </SubNavFilter>
+
+        <SubNavFilter>
+          Authors
+          <Dropdown
+            triggerText='All'
+            triggerTitle='Toggle menu options'
+            triggerElement={DropdownTrigger}>
+            <DropdownList role='menu'>
+              <DropdownItem>All</DropdownItem>
+            </DropdownList>
+          </Dropdown>
+        </SubNavFilter>
+
+        <SubNavFilter>
+          Topics
+          <Dropdown
+            triggerText='All'
+            triggerTitle='Toggle menu options'
+            triggerElement={DropdownTrigger}>
+            <DropdownList role='menu'>
+              <DropdownItem>All</DropdownItem>
+            </DropdownList>
+          </Dropdown>
+        </SubNavFilter>
+
+        <SubNavFilter>
+          Sort
+          <Dropdown
+            triggerText='Newest'
+            triggerTitle='Toggle menu options'
+            triggerElement={DropdownTrigger}>
+            <DropdownList role='menu'>
+              <DropdownItem>Newest</DropdownItem>
+            </DropdownList>
+          </Dropdown>
+        </SubNavFilter>
 
         <SubNavActions>
           <SubNavAction>Search</SubNavAction>
