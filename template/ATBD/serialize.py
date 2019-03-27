26495faf403a5c49d581fdaf2039c1721a726dc5
Alyssa <alyssadelaine@gmail.com>
2019-03-26 20:45:01 -0400

    Add title

--- template/ATBD/serialize.py
+++ template/ATBD/serialize.py
@@ -14,11 +14,10 @@ def toCamelCase(snake_str):
 def processWYSIWYG(element):
     print('element 2 is ' + str(element))
     to_return = ''
-    for sub in element['document']['nodes']:
-        print('sub is ' + str(sub))
-        if sub['type'] != 'image':
-            text = sub['nodes'][0]['leaves'][0]['text']
-            if sub['type'] == 'equation':
+    for node in element['document']['nodes']:
+        if node['type'] != 'image':
+            text = node['nodes'][0]['leaves'][0]['text']
+            if node['type'] == 'equation':
                 text = ' \\begin{equation} ' + text + ' \\end{equation} '
             to_return += text
         else:
@@ -32,22 +31,27 @@ def processVarList(element):
     latexDF = varDF.to_latex(index=False, bold_rows=True, escape = False)
     return latexDF
 
+def processATBD(element):
+    return element['title']
+
 mapVars = {
     'scientific_theory': processWYSIWYG,
     'scientific_theory_assumptions': processWYSIWYG,
     'mathematical_theory': processWYSIWYG,
     'mathematical_theory_assumptions': processWYSIWYG,
     'algorithm_input_variables': processVarList,
-    'algorithm_output_variables': processVarList
+    'algorithm_output_variables': processVarList,
+    'atbd': processATBD
 }
 
 def macroWrap(name, value):
     return '\\newcommand{{\\{fn}}}{{{val}}}'.format(fn=name, val=value)
 
-
 def helperTwo (name, element):
     print('name: {} || element: {}'.format(name, element))
-    if name in mapVars.keys() and element is not None:
+    if name == 'atbd':
+        return macroWrap('ATBDTitle', processATBD(element))
+    elif name in mapVars.keys() and element is not None:
         return macroWrap(toCamelCase(name), mapVars[name](element))
     elif element is None:
         return macroWrap(toCamelCase(name), 'Lorem Ipsum Filler text')
