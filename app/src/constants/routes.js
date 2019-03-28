1d291603d53188887bd8144c2a29067e20429eac
Sean Harkins <sharkinsgis@gmail.com>
2019-03-28 16:37:57 -0400

    Change versions route parameter to drafts. Closes #36.

--- app/src/constants/routes.js
+++ app/src/constants/routes.js
@@ -1,5 +1,5 @@
 export const atbds = 'atbds';
 export const atbdsedit = 'atbdsedit';
 export const contacts = 'contacts';
-export const versions = 'versions';
+export const drafts = 'drafts';
 export const algorithm_description = 'algorithm_description';
