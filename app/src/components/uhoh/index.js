cff7d7a77c00867dfe35154871512b00b0bb786e
Ricardo Mestre <ricardo@duplos.org>
2019-03-28 18:00:34 +0000

    Restructure and restyle 404

--- app/src/components/uhoh/index.js
+++ app/src/components/uhoh/index.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { antialiased } from '../../styles/helpers';
+import { themeVal } from '../../styles/utils/general';
+import { multiply } from '../../styles/utils/math';
+import Constrainer from '../../styles/atoms/Constrainer';
+
+const InpageHeaderInner = styled(Constrainer)`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-end;
+  padding: ${multiply(themeVal('layout.space'), 2)} ${themeVal('layout.space')} ${themeVal('layout.space')} ${themeVal('layout.space')};
+`;
+
+const Inpage = styled.article`
+
+`;
+
+const InpageHeader = styled.header`
+  ${antialiased()}
+  background-color: ${themeVal('color.primary')};
+  color: #FFF;
+`;
+
+const InpageTitle = styled.h1`
+  font-size: 1.25rem;
+  line-height: 1;
+  margin: 0;
+`;
+
+const InpageBody = styled.div`
+
+`;
+
+const InpageBodyInner = styled(Constrainer)`
+  padding: ${themeVal('layout.space')};
+`;
 
 class UhOh extends Component {
   render() {
     return (
-      <div>
-        <h1>Page not found</h1>
-        <div>
-          <p>We were not able to find the page you are looking for. It may have been archived or removed.</p>
-          <p>If you think this page should be here let us know via <a href='mailto:' title='Send us an email'>email</a>.</p>
-          <p><a href='/' title='Visit the homepage'>Visit the homepage</a>.</p>
-        </div>
-      </div>
+      <Inpage>
+        <InpageHeader>
+          <InpageHeaderInner>
+            <InpageTitle as="h1" variation="base">Page not found</InpageTitle>
+          </InpageHeaderInner>
+        </InpageHeader>
+        <InpageBody>
+          <InpageBodyInner>
+            <p>We were not able to find the page you are looking for. It may have been archived or removed.</p>
+            <p><a href='/' title='View page'>Visit the homepage</a>.</p>
+          </InpageBodyInner>
+        </InpageBody>
+      </Inpage>
     );
   }
 }
