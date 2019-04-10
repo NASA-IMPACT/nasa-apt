03819fc2495634509f47f4e50c549a50d071c092
Ricardo Mestre <ricardo@duplos.org>
2019-04-10 20:06:28 +0100

    Include info tooltip example

--- app/src/components/Contacts.js
+++ app/src/components/Contacts.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
+import styled from 'styled-components';
+import ReactTooltip from 'react-tooltip';
+
+import { themeVal } from '../styles/utils/general';
+import collecticon from '../styles/collecticons';
+
 import ContactForm from './ContactForm';
 import {
   Inpage
@@ -12,7 +19,18 @@ import EditPage, {
 import RemovableListItem from './common/RemovableListItem';
 import { createAtbdContact, deleteAtbdContact } from '../actions/actions';
 
+import FormGroup from '../styles/molecules/form/group';
+import FormToolbar from '../styles/molecules/form/toolbar';
+import FormLabel from '../styles/atoms/form/label';
+
+import Button from '../styles/atoms/button';
 import Select from './Select';
+
+const InfoButton = styled(Button)`
+  ::before {
+    ${collecticon('circle-information')}
+  }
+`;
 
 const Contacts = (props) => {
   const {
@@ -63,16 +81,21 @@ const Contacts = (props) => {
       >
         <h2>Contacts</h2>
         <EditorSection>
-          <EditorLabel>Existing contacts</EditorLabel>
-          <Select
-            name="existing-contact"
-            label="Select an existing contact"
-            options={contactOptions}
-            onChange={e => dispatchCreateAtbdContact({
-              atbd_id: selectedAtbd.atbd_id,
-              contact_id: e.target.value
-            })}
-          />
+          <FormGroup>
+            <FormLabel>Existing contacts</FormLabel>
+            <FormToolbar>
+              <InfoButton variation='base-plain' size='small' hideText data-tip='Lorem ipsum dolor sit amet.'>Learn more</InfoButton>
+              <ReactTooltip effect='solid' className='type-primary' />
+            </FormToolbar>
+            <Select
+              name="existing-contact"
+              options={contactOptions}
+              onChange={e => dispatchCreateAtbdContact({
+                atbd_id: selectedAtbd.atbd_id,
+                contact_id: e.target.value
+              })}
+            />
+          </FormGroup>
         </EditorSection>
 
         <EditorSection>
