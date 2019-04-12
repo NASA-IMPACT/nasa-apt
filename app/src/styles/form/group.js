6fde148d164aeeda176441856e7cee17f4d7627a
Ricardo Mestre <ricardo@duplos.org>
2019-04-12 17:45:25 +0100

    Extend form group components

--- app/src/styles/form/group.js
+++ app/src/styles/form/group.js
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 import { themeVal } from '../utils/general';
+import { divide } from '../utils/math';
 
-const FormGroup = styled.div`
+export const FormGroup = styled.div`
   display: grid;
-  grid-template-columns: 1fr auto;
   grid-template-rows: auto;
-  grid-template-areas:
-    "form-group-label form-group-toolbar"
-    "form-group-body form-group-body";
-  }
   grid-gap: ${themeVal('layout.space')};
-
-  > * {
-    grid-column-start: 1;
-    grid-column-end: -1;
-  }
 `;
 
-export default FormGroup;
+export const FormGroupHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FormGroupBody = styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-gap: ${divide(themeVal('layout.space'), 2)};
+`;
