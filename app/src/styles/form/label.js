9c719e1b225d95a2b22e98eaba435c107726acb3
Ricardo Mestre <ricardo@duplos.org>
2019-04-12 15:15:33 +0100

    Improve form label component
    Add optional prop.

--- app/src/styles/form/label.js
+++ app/src/styles/form/label.js
@@ -1,9 +1,36 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import T from 'prop-types';
 import styled from 'styled-components';
+
 import { themeVal } from '../utils/general';
 
-const FormLabel = styled.label`
+const LabelHint = styled.small`
+  font-weight: ${themeVal('type.base.weight')};
+  font-size: 0.75rem;
+  line-height: 1;
+  opacity: 0.64;
+`;
+
+const LabelBase = ({
+  children, optional, ...rest
+}) => (
+  <label {...rest}>
+    {children}
+    {optional && '\u00A0'}
+    {optional && (<LabelHint>(optional)</LabelHint>)}
+  </label>
+);
+
+LabelBase.propTypes = {
+  children: T.node,
+  optional: T.bool
+};
+
+const FormLabel = styled(LabelBase)`
   grid-area: form-group-label;
   display: inline-flex;
+  align-items: center;
   font-family: ${themeVal('type.base.family')};
   font-weight: ${themeVal('type.base.bold')};
   font-size: 1rem;
