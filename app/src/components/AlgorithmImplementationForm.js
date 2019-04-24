c6aa16fc1392013b0c6430aebc9abc864d035e30
Derek Lieu <dl@dereklieu.com>
2019-04-24 15:27:23 -0700

    Add validation messages

--- app/src/components/AlgorithmImplementationForm.js
+++ app/src/components/AlgorithmImplementationForm.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Value } from 'slate';
+import isUrl from 'is-url';
+import Plain from 'slate-plain-serializer';
 
 import FreeEditor from './FreeEditor';
 import {
@@ -14,43 +16,110 @@ import {
 import FormLegend from '../styles/form/legend';
 import FormLabel from '../styles/form/label';
 import FormInput from '../styles/form/input';
+import {
+  FormHelper,
+  FormHelperMessage
+} from '../styles/form/helper';
 import { getValidOrBlankDocument } from './editorBlankDocument';
 
-function AlgorithmImplementationForm(props) {
-  const {
-    id,
-    label,
-    accessUrl = '',
-    executionDescription,
-    save
-  } = props;
-  return (
-    <FormFieldset>
-      <FormGroup>
-        {!!label && <FormLegend>{label}</FormLegend>}
-        <FormGroupHeader>
-          <FormLabel htmlFor={`${id}-access`}>Access URL</FormLabel>
-        </FormGroupHeader>
-        <FormGroupBody>
-          <FormInput
-            type="text"
-            size="large"
-            id={`${id}-access`}
-            placeholder="Enter an access URL"
-            value={accessUrl}
+class AlgorithmImplementationForm extends Component {
+  constructor(props) {
+    super(props);
+    const { accessUrl = '' } = props;
+    this.state = {
+      accessUrl,
+      accessUrlInvalid: false,
+      executionDescriptionInvalid: false
+    };
+    this.onAccessUrlChange = this.onAccessUrlChange.bind(this);
+    this.onAccessUrlBlur = this.onAccessUrlBlur.bind(this);
+    this.onSave = this.onSave.bind(this);
+  }
+
+  onAccessUrlChange(e) {
+    this.setState({ accessUrl: e.currentTarget.value });
+  }
+
+  onAccessUrlBlur() {
+    const { accessUrl } = this.state;
+    if (accessUrl.length && !isUrl(accessUrl)) {
+      this.setState({ accessUrlInvalid: true });
+    } else {
+      this.setState({ accessUrlInvalid: false });
+    }
+  }
+
+  onSave(executionDescription) {
+    const { accessUrl } = this.state;
+    const accessUrlInvalid = !isUrl(accessUrl);
+    const executionDescriptionInvalid = !Plain.serialize(executionDescription).length;
+    this.setState({
+      accessUrlInvalid,
+      executionDescriptionInvalid
+    });
+  }
+
+  render() {
+    const {
+      id,
+      label,
+      executionDescription,
+      save
+    } = this.props;
+    const {
+      accessUrl,
+      accessUrlInvalid,
+      executionDescriptionInvalid
+    } = this.state;
+    const {
+      onAccessUrlChange,
+      onAccessUrlBlur,
+      onSave
+    } = this;
+    return (
+      <FormFieldset>
+        <FormGroup>
+          {!!label && <FormLegend>{label}</FormLegend>}
+          <FormGroupHeader>
+            <FormLabel htmlFor={`${id}-access`}>Access URL</FormLabel>
+            {accessUrlInvalid && (
+              <FormHelper>
+                <FormHelperMessage>Please enter a valid URL.</FormHelperMessage>
+              </FormHelper>
+            )}
+          </FormGroupHeader>
+          <FormGroupBody>
+            <FormInput
+              type="text"
+              size="large"
+              id={`${id}-access`}
+              placeholder="Enter an access URL"
+              value={accessUrl}
+              onChange={onAccessUrlChange}
+              onBlur={onAccessUrlBlur}
+              invalid={accessUrlInvalid}
+            />
+          </FormGroupBody>
+        </FormGroup>
+        <FormGroup>
+          <FormGroupHeader>
+            <FormLabel>Execution Description</FormLabel>
+            {executionDescriptionInvalid && (
+              <FormHelper>
+                <FormHelperMessage>This field is required.</FormHelperMessage>
+              </FormHelper>
+            )}
+          </FormGroupHeader>
+          <FreeEditor
+            value={Value.fromJSON(getValidOrBlankDocument(executionDescription))}
+            save={onSave}
+            invalid={executionDescriptionInvalid}
+            externalSaveBtn
           />
-        </FormGroupBody>
-      </FormGroup>
-      <FormGroup>
-        <FormGroupHeader>
-          <FormLabel>Execution Description</FormLabel>
-        </FormGroupHeader>
-        <FreeEditor
-          value={Value.fromJSON(getValidOrBlankDocument(executionDescription))}
-        />
-      </FormGroup>
-    </FormFieldset>
-  );
+        </FormGroup>
+      </FormFieldset>
+    );
+  }
 }
 
 AlgorithmImplementationForm.propTypes = {
--- app/src/components/Toolbars.js
+++ app/src/components/Toolbars.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { multiply } from '../styles/utils/math';
 import { themeVal } from '../styles/utils/general';
 import Button from '../styles/button/button';
 import collecticon from '../styles/collecticons';
@@ -27,8 +26,8 @@ export const Toolbar = styled.div`
   flex-flow: row nowrap;
   align-items: center;
   background: ${themeVal('color.shadow')};
-  border-top-left-radius: ${multiply(themeVal('layout.space'), 0.25)};
-  border-top-right-radius: ${multiply(themeVal('layout.space'), 0.25)};
+  border-top-left-radius: ${themeVal('shape.rounded')};
+  border-top-right-radius: ${themeVal('shape.rounded')};
   padding: 0 3rem;
 `;
 
