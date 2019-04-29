9f3d8be9405e2b71bdbcf0ed02996b6f092a05e7
Sean Harkins <sharkinsgis@gmail.com>
2019-04-29 19:49:31 -0400

    Add RDS specific code to init.sql.

--- init.sql
+++ init.sql
@@ -1,3 +1,13 @@
+CREATE ROLE app_user;
+GRANT app_user TO masteruser;
+CREATE SCHEMA apt;
+GRANT USAGE ON SCHEMA apt TO app_user;
+GRANT CONNECT ON DATABASE nasadb TO app_user;
+GRANT USAGE ON SCHEMA apt TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA apt TO app_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA apt GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
+SET SCHEMA 'apt';
+
 CREATE TYPE e_contact_mechanism_type AS ENUM (
  'Direct Line', 
  'Email', 
