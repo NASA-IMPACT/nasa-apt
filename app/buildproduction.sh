9cc496f4dfb25211cd3061e6c489baaaaae6cfaa
Sean Harkins <sharkinsgis@gmail.com>
2019-04-29 19:42:34 -0400

    Add production build script for front end to request CF stack values.

---
+++ app/buildproduction.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export REACT_APP_ATBD_JSON_BUCKET=$(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-AtbdJsonBucket\`].Value" --output text)
+echo $REACT_APP_ATBD_JSON_BUCKET
+export REACT_APP_ATBD_BUCKET=$(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-AtbdBucket\`].Value" --output text)
+echo $REACT_APP_ATBD_BUCKET
+export REACT_APP_API_URL=$(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-PostgRESTEndpoint\`].Value" --output text)
+echo $REACT_APP_API_URL
+yarn build
+
