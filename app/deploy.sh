5bc2a55f5229efb7b0e94349be59e11bb1796f3c
Sean Harkins <sharkinsgis@gmail.com>
2019-04-29 22:44:14 -0400

    Add static hosting bucket for front end application and deploy.

--- app/deploy.sh
+++ app/deploy.sh
@@ -6,4 +6,5 @@ echo $REACT_APP_ATBD_BUCKET
 export REACT_APP_API_URL=$(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-PostgRESTEndpoint\`].Value" --output text)
 echo $REACT_APP_API_URL
 yarn build
+aws s3 sync build/ $(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-WebsiteBucket\`].Value" --output text)
 
