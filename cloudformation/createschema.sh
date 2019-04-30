a3e8506c5557c28ee090b7087e2a9433e88e68b9
Sean Harkins <sharkinsgis@gmail.com>
2019-04-30 09:08:10 -0400

    Split user and schema creation into multiple sql files.

--- cloudformation/createschema.sh
+++ cloudformation/createschema.sh
@@ -1,1 +1,10 @@
-psql $(aws cloudformation list-exports --region us-east-1 --query "Exports[?Name==\`nasa-apt-PGConnection\`].Value" --output text) -f ../init.sql
+psql $(aws cloudformation list-exports --region us-east-1 --query \
+  "Exports[?Name==\`nasa-apt-PGConnection\`].Value" --output text) \
+  -f ./createdb.sql
+PGOPTIONS="--search_path=apt"
+export PGOPTIONS
+psql $(aws cloudformation list-exports --region us-east-1 --query \
+  "Exports[?Name==\`nasa-apt-PGConnection\`].Value" --output text) \
+  -f ../init.sql
+
+
