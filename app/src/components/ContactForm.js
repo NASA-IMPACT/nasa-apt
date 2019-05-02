924b2a5ae17424ee9be32c8a7b0f69cc0882f072
Derek Lieu <dl@dereklieu.com>
2019-05-01 21:58:12 -0700

    Adjust contact form to match mocks

--- app/src/components/ContactForm.js
+++ app/src/components/ContactForm.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withFormik } from 'formik';
 import jsonschema from 'jsonschema';
+import styled from 'styled-components';
 
-import { createContact } from '../actions/actions';
 import apiSchema from '../schemas/schema.json';
 import addMinLength from '../schemas/addMinLength';
 import transformErrors from '../schemas/transformErrors';
@@ -15,6 +15,13 @@ import Input, {
   InputFormGroup,
   InputSubmit
 } from './common/Input';
+import {
+  FormFieldset,
+  FormFieldsetHeader,
+  FormFieldsetBody
+} from '../styles/form/fieldset';
+import Form from '../styles/form/form';
+import FormLegend from '../styles/form/legend';
 
 const validator = new jsonschema.Validator();
 const contactsSchema = addMinLength(apiSchema.definitions.contacts);
@@ -24,11 +31,17 @@ contactsSchema.required = contactsSchema
 const first_name = 'first_name';
 const middle_name = 'middle_name';
 const last_name = 'last_name';
+const uuid = 'uuid';
+const url = 'url';
 const contact_mechanism_type = 'contact_mechanism_type';
 const contact_mechanism_types = contactsSchema
   .properties[contact_mechanism_type].enum;
 const contact_mechanism_value = 'contact_mechanism_value';
 const email = 'Email';
+
+const SpanTwo = styled.div`
+  grid-column-start: span 2;
+`;
 
 export const InnerContactForm = (props) => {
   const {
@@ -38,73 +51,126 @@ export const InnerContactForm = (props) => {
     handleChange,
     handleBlur,
     handleSubmit,
+    id,
+    contact,
+    t
   } = props;
   const submitEnabled = !Object.keys(errors).length
                                   && Object.keys(touched).length;
+
+  const submitValue = contact ? 'Update contact' : 'Create contact';
   return (
-    <InputFormGroup onSubmit={handleSubmit}>
-      <Input
-        id="cf-first-name"
-        name={first_name}
-        label="First Name"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values[first_name]}
-        error={errors[first_name]}
-        touched={touched[first_name]}
-        info="This is some info text. Awesome!"
-      />
-      <Input
-        id="cf-middle-name"
-        name={middle_name}
-        label="Middle Name"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values[middle_name]}
-        error={errors[middle_name]}
-        touched={touched[middle_name]}
-      />
-      <Input
-        id="cf-last-name"
-        name={last_name}
-        label="Last Name"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values[last_name]}
-        error={errors[last_name]}
-        touched={touched[last_name]}
-      />
-      <Select
-        id="cf-contact-type"
-        name={contact_mechanism_type}
-        label="Contact Type"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values[contact_mechanism_type]}
-        error={errors[contact_mechanism_type]}
-        touched={touched[contact_mechanism_type]}
-        options={contact_mechanism_types.map(d => ({ value: d, label: d }))}
-      />
-      <Input
-        id="cf-contact-mechanism-value"
-        name={contact_mechanism_value}
-        label="Contact"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values[contact_mechanism_value]}
-        error={errors[contact_mechanism_value]}
-        touched={touched[contact_mechanism_value]}
-      />
+    <Form onSubmit={handleSubmit}>
+
+      <InputFormGroup>
+        <Input
+          id={`${id}-first-name`}
+          name={first_name}
+          label="First Name"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[first_name]}
+          error={errors[first_name]}
+          touched={touched[first_name]}
+          info={t.person_first_name}
+        />
+        <Input
+          id={`${id}-middle-name`}
+          name={middle_name}
+          label="Middle Name"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[middle_name]}
+          error={errors[middle_name]}
+          touched={touched[middle_name]}
+          info={t.person_middle_name}
+        />
+        <Input
+          id={`${id}-last-name`}
+          name={last_name}
+          label="Last Name"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[last_name]}
+          error={errors[last_name]}
+          touched={touched[last_name]}
+          info={t.person_last_name}
+        />
+        <Input
+          id={`${id}-last-uuid`}
+          name={uuid}
+          label="UUID"
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[uuid]}
+          error={errors[uuid]}
+          touched={touched[uuid]}
+          info={t.person_uuid}
+          optional
+        />
+        <SpanTwo>
+          <Input
+            id={`${id}-url`}
+            name={url}
+            label="URL"
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[url]}
+            error={errors[url]}
+            touched={touched[url]}
+            info={t.person_related_url}
+            optional
+          />
+        </SpanTwo>
+      </InputFormGroup>
+
+      <FormFieldset>
+        <FormFieldsetHeader>
+          <FormLegend>Contact Mechanism</FormLegend>
+        </FormFieldsetHeader>
+        <FormFieldsetBody>
+          <InputFormGroup>
+            <Select
+              id={`${id}-contact-type`}
+              name={contact_mechanism_type}
+              label="Type"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values[contact_mechanism_type]}
+              error={errors[contact_mechanism_type]}
+              touched={touched[contact_mechanism_type]}
+              options={contact_mechanism_types.map(d => ({ value: d, label: d }))}
+              info={t.person_mechanism_type}
+            />
+            <SpanTwo>
+              <Input
+                id={`${id}-contact-mechanism-value`}
+                name={contact_mechanism_value}
+                label="Value"
+                type="text"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values[contact_mechanism_value]}
+                error={errors[contact_mechanism_value]}
+                touched={touched[contact_mechanism_value]}
+                info={t.person_mechanism_value}
+              />
+            </SpanTwo>
+          </InputFormGroup>
+        </FormFieldsetBody>
+      </FormFieldset>
+
       <InputSubmit
         type="submit"
         disabled={!submitEnabled}
-        value="Create Contact"
+        value={submitValue}
       />
-    </InputFormGroup>
+    </Form>
   );
 };
 
@@ -114,15 +180,25 @@ InnerContactForm.propTypes = {
   errors: PropTypes.object.isRequired,
   handleChange: PropTypes.func.isRequired,
   handleBlur: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+
+  contact: PropTypes.object,
+  id: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]).isRequired,
+  t: PropTypes.object
 };
 
 export const ContactForm = withFormik({
-  mapPropsToValues: () => {
+  mapPropsToValues: (props) => {
+    const contact = props.contact || {};
     const initialValues = {
-      [first_name]: '',
-      [middle_name]: '',
-      [last_name]: '',
+      [first_name]: contact.first_name || '',
+      [middle_name]: contact.middle_name || '',
+      [last_name]: contact.last_name || '',
+      [uuid]: contact.uuid || '',
+      [url]: contact.url || '',
       [contact_mechanism_type]: email,
       [contact_mechanism_value]: ''
     };
@@ -144,13 +220,16 @@ export const ContactForm = withFormik({
   },
 
   handleSubmit: (values, { props, setSubmitting, resetForm }) => {
-    const { createContact: create } = props;
-    create(values);
+    console.log(props);
     setSubmitting(false);
     resetForm();
   }
 })(InnerContactForm);
 
-const mapDispatchToProps = { createContact };
+const mapStateToProps = state => ({
+  t: state.application.t ? state.application.t.contact_information : {}
+});
 
-export default connect(null, mapDispatchToProps)(ContactForm);
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
