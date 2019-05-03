8f26112557be62c408b67ee1ad497bae995f1dc7
Alyssa <alyssadelaine@gmail.com>
2019-05-03 15:18:36 -0400

    Save .tex file as public read object and echo success to log

--- ecs/tex/entrypoint.sh
+++ ecs/tex/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 aws s3 cp s3://$1/$2 .
 texName=$(python3 serialize.py $2)
-aws s3 cp $texName s3://$3/$texName
+aws s3 cp $texName s3://$3/$texName --acl public-read
+
+echo 'Successfully saved to s3://'$3'/'$texName
