a78b75e4e09d8ec6a8cd3ac676faaed9d841fc11
Sean Harkins <sharkinsgis@gmail.com>
2019-05-06 16:36:20 -0400

    Include website for atbd bucket so ui can reference html as static page.

--- app/deploy.sh
+++ app/deploy.sh
@@ -19,6 +19,10 @@ export REACT_APP_S3_URI=$(aws cloudformation list-exports --region \
   us-east-1 --query "Exports[?Name==\`nasa-apt-S3Endpoint\`].Value" \
   --output text)
 echo $REACT_APP_S3_URI
+export REACT_APP_ATBD_BUCKET_WEBSITE=$(aws cloudformation list-exports --region \
+  us-east-1 --query "Exports[?Name==\`nasa-apt-AtbdBucketWebsite\`].Value" \
+  --output text)
+echo $REACT_APP_S3_URI
 yarn build
 aws s3 sync build/ $(aws cloudformation list-exports --region us-east-1 \
   --query "Exports[?Name==\`nasa-apt-WebsiteBucket\`].Value" --output text)
