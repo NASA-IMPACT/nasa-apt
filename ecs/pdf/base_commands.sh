60f9c9ce42f7c6f4b1f07787a1d5fc1d56b27ad4
Alyssa <alyssadelaine@gmail.com>
2019-05-06 11:29:22 -0400

    Include package allowing strikethroughs

--- ecs/pdf/base_commands.sh
+++ ecs/pdf/base_commands.sh
@@ -4,5 +4,6 @@ apt-get install python3-pip --assume-yes
 pip3 install --upgrade awscli
 
 apt-get install -y texlive-latex-recommended
+apt-get install -y texlive-generic-recommended
 
 apt-get --assume-yes install wget
