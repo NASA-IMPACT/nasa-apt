8e4312afca9be1969f331cc0173134f00464da07
Alyssa <alyssadelaine@gmail.com>
2019-05-06 17:54:27 -0400

    More specific logging and add in image size control to .css file used in html

--- ecs/tex/entrypoint.sh
+++ ecs/tex/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 aws s3 cp s3://$1/$2 .
+
+if [ $? -eq 0 ]; then
+    echo 'Successfully retrieved JSON file from s3://'$1'/'$2
+else
+    echo 'Failed to retrieve original JSON file'
+fi
+
 texName=$(python3 serialize.py $2)
+
+if [ $? -eq 0 ]; then
+    echo 'Successfully wrote TeX file'$texName
+else
+    echo 'Failed to write TeX file'
+fi
+
 aws s3 cp $texName s3://$3/$texName --acl public-read
 
-echo 'Successfully saved to s3://'$3'/'$texName
+if [ $? -eq 0 ]; then
+    echo 'Successfully saved to s3://'$3'/'$texName
+else
+    echo 'Failed to write to s3'
+fi
