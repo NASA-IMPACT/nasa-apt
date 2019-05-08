262a67209bbf269fc80164b6d8df00d66dfed35c
Derek Lieu <dl@dereklieu.com>
2019-05-07 22:03:39 -0700

    Underline references

--- app/src/components/EditorReference.js
+++ app/src/components/EditorReference.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components/macro';
 
 const Reference = styled.sup`
+  margin-left: 0.1rem;
+  text-decoration: underline;
+  * {
+    text-decoration: none;
+  }
 `;
 
 export default Reference;
