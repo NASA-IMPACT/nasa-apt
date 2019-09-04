2b011c1a215dfe05ab5d3908a587d0c3eba0f6e1
Sean Harkins <sharkinsgis@gmail.com>
2019-09-04 17:05:44 -0400

    Add title as text searchable field.

--- db/deploy/textsearch.sql
+++ db/deploy/textsearch.sql
@@ -11,8 +11,10 @@ CREATE FUNCTION apt.search_text(searchstring text) returns SETOF apt.atbds
     apt.atbds.atbd_id,
     apt.atbds.title
   FROM apt.atbds
-  INNER JOIN apt.atbd_contacts ON apt.atbd_contacts.atbd_id = apt.atbds.atbd_id
-  INNER JOIN apt.contacts ON apt.contacts.contact_id = apt.atbd_contacts.contact_id
-  WHERE to_tsvector(apt.contacts.first_name) @@ to_tsquery(searchstring);
+  LEFT JOIN apt.atbd_contacts ON apt.atbd_contacts.atbd_id = apt.atbds.atbd_id
+  LEFT JOIN apt.contacts ON apt.contacts.contact_id = apt.atbd_contacts.contact_id
+  WHERE to_tsvector(apt.contacts.first_name) @@ to_tsquery(searchstring)
+  OR to_tsvector(apt.contacts.last_name) @@ to_tsquery(searchstring)
+  OR to_tsvector(apt.atbds.title) @@ to_tsquery(searchstring);
   $_$;
 COMMIT;
