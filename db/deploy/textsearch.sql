f72e42d9fba3461fe438bb82da9b1ef93c0dbf99
Sean Harkins <sharkinsgis@gmail.com>
2019-09-04 20:10:40 -0400

    Update join structure to return distinct records from search_text.

--- db/deploy/textsearch.sql
+++ db/deploy/textsearch.sql
@@ -7,14 +7,13 @@ CREATE FUNCTION apt.search_text(searchstring text) returns SETOF apt.atbds
   LANGUAGE sql
   IMMUTABLE
   AS $_$
-  SELECT
+  SELECT DISTINCT
     apt.atbds.atbd_id,
     apt.atbds.title
   FROM apt.atbds
-  LEFT JOIN apt.atbd_contacts ON apt.atbd_contacts.atbd_id = apt.atbds.atbd_id
-  LEFT JOIN apt.contacts ON apt.contacts.contact_id = apt.atbd_contacts.contact_id
-  WHERE to_tsvector(apt.contacts.first_name) @@ to_tsquery(searchstring)
-  OR to_tsvector(apt.contacts.last_name) @@ to_tsquery(searchstring)
+  LEFT OUTER JOIN apt.atbd_contacts ON apt.atbd_contacts.atbd_id = apt.atbds.atbd_id
+  LEFT OUTER JOIN apt.contacts ON apt.contacts.contact_id = apt.atbd_contacts.contact_id
+  WHERE to_tsvector(CONCAT(apt.contacts.first_name, ' ', apt.contacts.last_name)) @@ to_tsquery(searchstring)
   OR to_tsvector(apt.atbds.title) @@ to_tsquery(searchstring);
   $_$;
 COMMIT;
