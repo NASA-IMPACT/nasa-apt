707948cda21d60c62f0c5b13b7c5669a2c1e533c
Sean Harkins <sharkinsgis@gmail.com>
2019-09-06 18:01:18 -0400

    Restructure output type to match fetchAtbds.

--- db/deploy/textsearch.sql
+++ db/deploy/textsearch.sql
@@ -18,7 +18,7 @@
   --  $_$;
 --  COMMIT;
 BEGIN;
-CREATE TYPE apt.atbd_reduced_version AS (
+CREATE TYPE apt.atbd_reduced_versions AS (
   atbd_id INTEGER,
   atbd_version INTEGER,
   status apt.atbd_status
@@ -26,7 +26,8 @@ CREATE TYPE apt.atbd_reduced_version AS (
 CREATE TYPE apt.atbd_reduced AS (
   atbd_id INTEGER,
   title VARCHAR (1024),
-  contacts apt.contacts[]
+  contacts apt.contacts[],
+  atbd_versions apt.atbd_reduced_versions[]
 );
 
 CREATE FUNCTION apt.search_text(searchstring text) returns SETOF apt.atbd_reduced
@@ -36,8 +37,15 @@ CREATE FUNCTION apt.search_text(searchstring text) returns SETOF apt.atbd_reduce
   SELECT
     apt.atbds.atbd_id,
     apt.atbds.title,
-    array_agg(apt.contacts.*) as contacts
+    array_agg(apt.contacts.*) as contacts,
+    array_agg(
+      ROW(
+        apt.atbd_versions.atbd_id, apt.atbd_versions.atbd_version, apt.atbd_versions.status)
+        ::apt.atbd_reduced_versions
+      )
+        AS atbd_versions
   FROM apt.atbds
+  FULL OUTER JOIN apt.atbd_versions ON apt.atbd_versions.atbd_id = apt.atbds.atbd_id
   FULL OUTER JOIN apt.atbd_contacts ON apt.atbd_contacts.atbd_id = apt.atbds.atbd_id
   FULL OUTER JOIN apt.contacts ON apt.contacts.contact_id = apt.atbd_contacts.contact_id
   WHERE apt.atbds.title LIKE searchstring
