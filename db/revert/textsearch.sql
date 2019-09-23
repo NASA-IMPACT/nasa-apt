d569c668c5d52af81297c17c89a732c3307fb19e
Sean Harkins <sharkinsgis@gmail.com>
2019-09-23 17:23:24 -0400

    Add valid Sqitch revert for textsearch.

--- db/revert/textsearch.sql
+++ db/revert/textsearch.sql
@@ -1,6 +1,6 @@
 -- Revert nasa-apt:textsearch from pg
 BEGIN;
-DROP FUNCTION apt.search_text(searchstring text);
+DROP FUNCTION apt.search_text(searchstring text, statusstring text);
 DROP TYPE apt.atbd_reduced;
 DROP TYPE apt.atbd_reduced_versions;
 COMMIT;
