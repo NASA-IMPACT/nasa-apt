28727427834b726d242d725791d29db420b6ec36
Alyssa <alyssadelaine@gmail.com>
2019-10-02 15:24:18 -0400

    Function to duplicate ATBD

--- db/deploy/functions.sql
+++ db/deploy/functions.sql
@@ -1,6 +1,42 @@
 -- Deploy nasa-apt:functions to pg
 -- requires: tables
 
+CREATE FUNCTION apt.copy_atbd_version(val integer, OUT created_atbd apt.atbds, OUT created_version apt.atbd_versions)
+ AS $$
+  BEGIN
+  INSERT INTO apt.atbds(title) select title from apt.atbds where atbd_id = val RETURNING * INTO created_atbd;
+  INSERT INTO apt.atbd_versions(
+	atbd_id, 
+	atbd_version,
+    status,
+	scientific_theory,
+    scientific_theory_assumptions,
+    mathematical_theory,
+    mathematical_theory_assumptions,
+    introduction,
+    historical_perspective,
+    performance_assessment_validation_methods,
+    performance_assessment_validation_uncertainties,
+    performance_assessment_validation_errors,
+    algorithm_usage_constraints)
+  	SELECT
+		created_atbd.atbd_id, 1, 'Draft',
+		scientific_theory,
+	    scientific_theory_assumptions,
+	    mathematical_theory,
+	    mathematical_theory_assumptions,
+	    introduction,
+	    historical_perspective,
+	    performance_assessment_validation_methods,
+	    performance_assessment_validation_uncertainties,
+	    performance_assessment_validation_errors,
+	    algorithm_usage_constraints
+    FROM apt.atbd_versions where atbd_id = val
+	RETURNING * INTO created_version;
+  END; $$
+LANGUAGE PLPGSQL;
+
+
 BEGIN;
 CREATE FUNCTION apt.create_atbd_version(OUT created_atbd apt.atbds, OUT created_version apt.atbd_versions)
   AS $$
