acb8dc2de9c41d42fc443c820e3d992bfe694e5d
Alyssa <alyssadelaine@gmail.com>
2019-10-18 14:07:45 -0400

    Make function volatile and update variable name

--- db/deploy/copyATBD.sql
+++ db/deploy/copyATBD.sql
@@ -5,7 +5,7 @@ BEGIN;
 
 CREATE EXTENSION hstore;
 
-CREATE FUNCTION apt.copy_atbd(orig_id integer, OUT new_id integer, OUT created_atbd apt.atbds)
+CREATE FUNCTION apt.copy_atbd(original_id integer, OUT new_id integer, OUT created_atbd apt.atbds)
 AS $$
 BEGIN
 new_id := (select nextval(pg_get_serial_sequence('apt.atbds', 'atbd_id')));
@@ -13,74 +13,74 @@ INSERT INTO apt.atbds
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.atbds t WHERE atbd_id = orig_id
+   FROM   apt.atbds t WHERE atbd_id = original_id
    ) sub RETURNING * INTO created_atbd;
 
 INSERT INTO apt.atbd_versions
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.atbd_versions t WHERE atbd_id = orig_id
+   FROM   apt.atbd_versions t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.citations
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.citations t WHERE atbd_id = orig_id
+   FROM   apt.citations t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.data_access_input_data
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.data_access_input_data t WHERE atbd_id = orig_id
+   FROM   apt.data_access_input_data t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.data_access_output_data
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.data_access_output_data t WHERE atbd_id = orig_id
+   FROM   apt.data_access_output_data t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.data_access_related_urls
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.data_access_related_urls t WHERE atbd_id = orig_id
+   FROM   apt.data_access_related_urls t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.publication_references
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.publication_references t WHERE atbd_id = orig_id
+   FROM   apt.publication_references t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.algorithm_implementations
 SELECT (t1).*
 FROM  (
    SELECT t #= hstore('atbd_id', new_id::text) AS t1
-   FROM   apt.algorithm_implementations t WHERE atbd_id = orig_id
+   FROM   apt.algorithm_implementations t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.algorithm_input_variables
 SELECT (t1).*
 FROM  (
    SELECT t #= (hstore('atbd_id', new_id::text) || hstore('algorithm_input_variable_id', nextval(pg_get_serial_sequence('apt.algorithm_input_variables', 'algorithm_input_variable_id'))::text)) AS t1
-   FROM   apt.algorithm_input_variables t WHERE atbd_id = orig_id
+   FROM   apt.algorithm_input_variables t WHERE atbd_id = original_id
    ) sub;
 
 INSERT INTO apt.algorithm_output_variables
 SELECT (t1).*
 FROM  (
    SELECT t #= (hstore('atbd_id', new_id::text) || hstore('algorithm_output_variable_id', nextval(pg_get_serial_sequence('apt.algorithm_output_variables', 'algorithm_output_variable_id'))::text)) AS t1
-   FROM   apt.algorithm_output_variables t WHERE atbd_id = orig_id
+   FROM   apt.algorithm_output_variables t WHERE atbd_id = original_id
    ) sub;
 
 END;
 $$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL VOLATILE;
 
 COMMIT;
