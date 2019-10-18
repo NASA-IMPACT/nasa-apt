dbd79dc6cfcbed4df6a49b859adfaef278282cb4
Alyssa <alyssadelaine@gmail.com>
2019-10-18 16:47:13 -0400

    Small edits to try to get copy_atbd working

--- db/revert/copyATBD.sql
+++ db/revert/copyATBD.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-DROP FUNCTION apt.copy_atbd(orig_id integer, OUT new_id integer, OUT created_atbd apt.atbds);
+DROP FUNCTION apt.copy_atbd(originalid integer, OUT new_id integer, OUT created_atbd apt.atbds);
 DROP EXTENSION hstore;
 
 COMMIT;
