c40110079b6fa4f180bc49cc47a9b8da9259cce9
Daniel da Silva <danielfdsilva@gmail.com>
2020-12-16 19:19:37 +0000

    Fix cloudformation variables

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -5,6 +5,25 @@ set -e
 read -p 'Stack name: ' stackname
 read -sp 'Database password: ' dbpassword
 
+source ../.env
+
+echo "Using env variables"
+echo "APT_FRONTEND_URL $APT_FRONTEND_URL"
+echo "IDP_METADATA_URL $IDP_METADATA_URL"
+echo "JWT_SECRET $JWT_SECRET"
+echo "FASTAPI_HOST $FASTAPI_HOST"
+
+echo
+
+read -p "Continue (y/n)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+echo "Deploying"
+
 aws cloudformation deploy \
   --template-file cloudformation.yaml \
   --stack-name $stackname \
@@ -13,5 +32,9 @@ aws cloudformation deploy \
       DBName=nasadb \
       DBUser=masteruser \
       DBPassword=$dbpassword \
+      JWTSecret=$JWT_SECRET \
+      APTFrontendUrl=$APT_FRONTEND_URL \
+      IDPMetadataUrl=$IDP_METADATA_URL \
+      FastapiHost=$FASTAPI_HOST \
       ElasticsearchDomainName=nasadb-$stackname \
   --region us-east-1 --capabilities CAPABILITY_IAM
