1be564f4d34225c0cd1295f3bc0c0c145a83666f
Leo Thomas <leo.thomas@mail.mcgill.ca>
2021-07-20 20:02:43 +0200

    Added first attempt at filterable /users endpoint

--- app/api/v2/users.py
+++ app/api/v2/users.py
@@ -2,9 +2,9 @@
 from typing import List
 
 from app.api.utils import (
-    get_major_from_version_string,
     get_active_user_principals,
     get_db,
+    get_major_from_version_string,
     list_cognito_users,
     require_user,
 )
@@ -12,6 +12,7 @@ from app.crud.atbds import crud_atbds
 from app.db.db_session import DbSession
 from app.schemas import users
 from app.schemas.users import User
+
 import fastapi_permissions as permissions
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -26,7 +27,7 @@ router = APIRouter()
 )
 def list_users(
     atbd_id: str,
-    major: str,
+    version: str,
     user_filter: str,
     db: DbSession = Depends(get_db),
     user: User = Depends(require_user),
@@ -35,7 +36,7 @@ def list_users(
     """
     Lists Users
     """
-    major = get_major_from_version_string(major)
+    major, _ = get_major_from_version_string(version)
     [atbd_version] = crud_atbds.get(db=db, atbd_id=atbd_id, version=major).versions
     version_acl = atbd_version.__acl__()
     app_users = list_cognito_users()
@@ -52,6 +53,34 @@ def list_users(
             for user in app_users
             if permissions.has_permission(
                 get_active_user_principals(user), "recieve_ownership", version_acl
+            )
+        ]
+    if user_filter == "invite_authors":
+        if not permissions.has_permission(principals, "invite_authors", version_acl):
+            raise HTTPException(
+                status_code=400,
+                detail=f"User {user['preferred_username']} is not allowed to invite authors",
+            )
+
+        eligible_users = [
+            user
+            for user in app_users
+            if permissions.has_permission(
+                get_active_user_principals(user), "join_authors", version_acl
+            )
+        ]
+    if user_filter == "invite_reviewers":
+        if not permissions.has_permission(principals, "invite_reviewers", version_acl):
+            raise HTTPException(
+                status_code=400,
+                detail=f"User {user['preferred_username']} is not allowed to invite reviewers",
+            )
+
+        eligible_users = [
+            user
+            for user in app_users
+            if permissions.has_permission(
+                get_active_user_principals(user), "join_reviewers", version_acl
             )
         ]
 
