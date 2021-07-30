8e7b3ea4d1a3dedd2888084100e6ccdb9deee4e8
Drew Bollinger <drew@developmentseed.org>
2021-07-30 12:06:01 -0400

    add document section constraint enum

--- db/deploy/threads.sql
+++ db/deploy/threads.sql
@@ -7,12 +7,32 @@ CREATE TYPE apt.thread_status AS ENUM (
   'Closed'
 );
 
+CREATE TYPE apt.document_section AS ENUM (
+  'general',
+  'citation',
+  'contacts',
+  'introduction',
+  'historical_perspective',
+  'scientific_theory',
+  'mathematical_theory',
+  'input_variables',
+  'output_variables',
+  'constraints',
+  'validation',
+  'algorithm_implementations',
+  'data_access_input_data',
+  'data_access_output_data',
+  'data_access_related_urls',
+  'discussion',
+  'acknowledgements'
+);
+
 CREATE TABLE apt.threads (
     id serial PRIMARY KEY,
     atbd_id integer NOT NULL,
     major integer NOT NULL,
     "status" apt.thread_status DEFAULT 'Open',
-    section VARCHAR(1024), 
+    "section" apt.document_section NOT NULL, 
     FOREIGN KEY (atbd_id, major) REFERENCES apt.atbd_versions (atbd_id, major) ON DELETE CASCADE
 );
 
