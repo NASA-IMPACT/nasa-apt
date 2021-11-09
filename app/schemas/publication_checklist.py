b54d7c398e5f3a40697b51223f92ce15c595ffc0
Leo Thomas <leo.thomas@mail.mcgill.ca>
2021-11-08 21:32:30 -0500

    Added default values for review_roles and author_affiliations

--- app/schemas/publication_checklist.py
+++ app/schemas/publication_checklist.py
@@ -1,10 +1,15 @@
-"""Models for the Versions.publication_checklist field"""
+"""
+Models for the Versions.publication_checklist field
+TODO: does this have to be a separate file? Can we
+include it in `./version.py`
+"""
 
 from __future__ import annotations
 
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, validator
+
 
 class SuggestedReviewer(BaseModel):
     """Suggested Reviewer"""
@@ -12,17 +17,15 @@ class SuggestedReviewer(BaseModel):
     name: str
     email: str
 
+
 class PublicationChecklist(BaseModel):
     """Top level `publication_checklist` node"""
 
     suggested_reviewers: Optional[List[SuggestedReviewer]]
-    review_roles: bool
+    review_roles: bool = False
     journal_editor: str = "Chelle Gentemann"
-    author_affirmations: bool
+    author_affiliations: bool = False
 
-    @validator(
-        "suggested_reviewers",
-        whole=True,
-    )
+    @validator("suggested_reviewers", whole=True)
     def _check_if_list_has_value(cls, value):
         return value or None
