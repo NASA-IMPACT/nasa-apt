c0023f9f1651e73ffa9e926111058623a2a73e75
Matt Robinson <pacificrobinson@gmail.com>
2022-07-29 15:44:02 +0200

    modified elastic search constructs to fit new open search constructs

--- stack/app.py
+++ stack/app.py
@@ -9,7 +9,7 @@ from aws_cdk import aws_apigatewayv2 as apigw
 from aws_cdk import aws_apigatewayv2_integrations as apigw_integrations
 from aws_cdk import aws_cognito as cognito
 from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_elasticsearch as elasticsearch
+from aws_cdk import aws_opensearchservice as opensearch
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_rds as rds
@@ -158,18 +158,18 @@ class nasaAPTLambdaStack(core.Stack):
             bucket_params["bucket_name"] = config.S3_BUCKET
         bucket = s3.Bucket(**bucket_params)
 
-        esdomain = elasticsearch.Domain(
+        opensearch_domain = opensearch.Domain(
             self,
-            f"{id}-elasticsearch-domain",
-            version=elasticsearch.ElasticsearchVersion.V7_7,
-            capacity=elasticsearch.CapacityConfig(
-                data_node_instance_type="t2.small.elasticsearch",
+            f"{id}-opensearch-domain",
+            version=opensearch.EngineVerision.OPENSEARCH_1_3,
+            capacity=opensearch.CapacityConfig(
+                data_node_instance_type="t2.small.search",
                 data_nodes=1,
             ),
-            # slice last 28 chars since Elastic Domains can't have a name longer than 28 chars in
+            # slice last 28 chars since Open Search Domains can't have a name longer than 28 chars in
             # AWS (and can't start with a `-` character)
-            domain_name=f"{id}-elastic"[-28:].strip("-"),
-            ebs=elasticsearch.EbsOptions(
+            domain_name=f"{id}-opensearch"[-28:].strip("-"),
+            ebs=opensearch.EbsOptions(
                 enabled=True,
                 iops=0,
                 volume_size=10,
@@ -190,7 +190,7 @@ class nasaAPTLambdaStack(core.Stack):
             APT_FRONTEND_URL=frontend_url,
             BACKEND_CORS_ORIGINS=config.BACKEND_CORS_ORIGINS,
             POSTGRES_ADMIN_CREDENTIALS_ARN=database.secret.secret_arn,
-            ELASTICSEARCH_URL=esdomain.domain_endpoint,
+            OPENSEARCH_URL=opensearch_domain.domain_endpoint,
             S3_BUCKET=bucket.bucket_name,
             NOTIFICATIONS_FROM=config.NOTIFICATIONS_FROM,
             MODULE_NAME="nasa_apt.main",
@@ -222,7 +222,7 @@ class nasaAPTLambdaStack(core.Stack):
         )
         lambda_function.add_to_role_policy(ses_access)
         database.secret.grant_read(lambda_function)
-        esdomain.grant_read_write(lambda_function)
+        opensearch_domain.grant_read_write(lambda_function)
         bucket.grant_read_write(lambda_function)
 
         # defines an API Gateway Http API resource backed by our custom lambda function.
