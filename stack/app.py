28fabd88355f68a54bdae6f29b34958d0edfcbf6
Tarashish Mishra <sunu@sunu.in>
2023-06-30 12:27:13 +0530

    assign the vpc instance and not the id

--- stack/app.py
+++ stack/app.py
@@ -207,10 +207,10 @@ class nasaAPTLambdaStack(core.Stack):
             removal_policy=core.RemovalPolicy.RETAIN
             if "prod" in config.STAGE.lower()
             else core.RemovalPolicy.DESTROY,
-            vpc=config.VPC_ID,
-            vpc_subnets=[
-                ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT)
-            ],
+            vpc=vpc,
+            # vpc_subnets=[
+            #     ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT)
+            # ],
             security_groups=[os_vpc_security_group],
         )
 
