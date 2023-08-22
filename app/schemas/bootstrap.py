7fbaa648a44de1c8e2ca569020bea3e780d13e5f
Tarashish Mishra <sunu@users.noreply.github.com>
2023-08-22 15:22:46 +0530

    Expose feature flags through an api endpoint (#797)
    * feature flags endpoint
    
    feature flags and metadata will be available for the frontend at /bootstrap endpoint
    
    * enable journal pdf export feature flag on staging
    
    * schema for feature flags

---
+++ app/schemas/bootstrap.py
@@ -0,0 +1,17 @@
+"""Schema for frontend bootstrap data."""
+
+from pydantic import BaseModel
+
+
+class FeatureFlags(BaseModel):
+    """Feature Flags."""
+
+    TWOFA_ENABLED: bool
+    JOURNAL_PDF_EXPORT_ENABLED: bool
+
+
+class BootstrapResponse(BaseModel):
+    """Bootstrap Response."""
+
+    metadata: dict
+    feature_flags: FeatureFlags
