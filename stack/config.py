b73ca1d0d723b96f4a5e69fdfc451e62adb90cbe
Tarashish Mishra <sunu@sunu.in>
2024-09-11 17:49:01 +0530

    Update frontend url; remove surge.sh

--- stack/config.py
+++ stack/config.py
@@ -55,7 +55,7 @@ FRONTEND_URL = os.environ.get("APT_FRONTEND_URL") or exit(
 )
 
 # if STAGE != "prod":
-#    FRONTEND_URL = f"{FRONTEND_URL},http://localhost:9000,https://nasa-apt.surge.sh"
+#    FRONTEND_URL = f"{FRONTEND_URL},http://localhost:9000"
 
 BACKEND_CORS_ORIGINS = os.environ.get(
     "BACKEND_CORS_ORIGINS",
